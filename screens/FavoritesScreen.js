19405f617a38ab5a1ac26952edcb14a4f053eca0
Alexandre Finger <alexandre.finger@esales.com.br>
2020-03-15 11:29:04 -0300

    Project finished

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector } from "react-redux";
+import HeaderButton from "../components/HeaderButton";
+import MealList from "../components/MealList";
+import Colors from '../constants/Colors'
+
+const FavoritesScreen = props => {
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <Text style={styles.textColor}>
+          No favorite meals founds. Start adding some!
+        </Text>
+      </View>
+    );
+  }
+
+  return <MealList listData={favMeals} navigation={props.navigation} />;
+};
+
+FavoritesScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Your Favorites",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textColor: {
+    color: Colors.textColor,
+    fontSize: 16
+  }
+});
+export default FavoritesScreen;
